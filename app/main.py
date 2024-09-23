263f7631e402b21b3517ec1e38a36918e29de7f8
Otto Ahonen <otto@priimus.consulting>
2024-09-23 19:07:41 +0300

    main.py: reorganize

--- app/main.py
+++ app/main.py
@@ -19,8 +19,6 @@ from .twilio_client import Twilio, TwilioInboundMessage
 
 
 DEBUG = bool(os.environ.get("SNOOKER_DEBUG", False))
-
-
 SETTINGS = get_settings()
 
 
@@ -34,6 +32,7 @@ def setup_logging():
 
 
 app = FastAPI()
+TWILIO = Twilio()
 
 
 async def parse_twilio_msg(req: Request) -> TwilioInboundMessage:
@@ -51,7 +50,37 @@ async def parse_twilio_msg(req: Request) -> TwilioInboundMessage:
     return TwilioInboundMessage(body=body, sender=sender, is_test=is_test)
 
 
-twilio = Twilio()
+async def handle_scores(msg: TwilioInboundMessage, settings):
+    sheet = SnookerSheet(settings.SHEETID)
+    llm = SnookerScoresLLM(llm=settings.LLM)
+    """Handles inbound scores"""
+    logging.info("Received message from %s: %s", msg.sender, msg.body)
+    valid_players = sheet.get_current_players()
+    try:
+        output: dict = llm.infer(passage=msg.body, valid_players_txt=sheet.players_txt)
+        snooker_match = get_match_model(valid_players=valid_players, max_score=settings.MAX_SCORE, **output)
+    except ValidationError as err:
+        TWILIO.send_message(msg.sender, messages.INVALID)
+        error_messages: list[str] = [err.get("msg") for err in err.errors()]
+        detail = {"llm_output": output, "error_messages": error_messages}
+        logging.error(json.dumps(detail))
+        raise HTTPException(status.HTTP_400_BAD_REQUEST, detail)
+    sheet.record_match(values=snooker_match.model_dump(), passage=msg.body, sender=msg.sender)
+    for break_ in snooker_match.breaks:
+        sheet.record_break(break_.model_dump(), passage=msg.body, sender=msg.sender)
+    reply = snooker_match.summary(snooker_match.passage_language)
+    TWILIO.send_message(msg.sender, reply)
+    reply_msg = "Match tested" if msg.is_test else "Match recorded"
+    content = {"status": reply_msg, "match": jsonable_encoder(snooker_match)}
+    logging.info(json.dumps(content))
+    return JSONResponse(status_code=status.HTTP_201_CREATED, content=content)
+
+
+@app.exception_handler(Exception)
+async def handle_exception(req: Request, exc: Exception):
+    logging.exception(exc)
+    if not isinstance(exc, HTTPException):
+        return JSONResponse(status_code=500, content={"detail": "Internal server error"})
 
 
 @app.post("/scores")
@@ -70,32 +99,6 @@ async def post_scores_sixred24(
     return await handle_scores(settings=get_settings(sixred24=True), msg=msg)
 
 
-async def handle_scores(msg: TwilioInboundMessage, settings):
-    sheet = SnookerSheet(settings.SHEETID)
-    llm = SnookerScoresLLM(llm=settings.LLM)
-    """Handles inbound scores"""
-    logging.info("Received message from %s: %s", msg.sender, msg.body)
-    valid_players = sheet.get_current_players()
-    try:
-        output: dict = llm.infer(passage=msg.body, valid_players_txt=sheet.players_txt)
-        snooker_match = get_match_model(valid_players=valid_players, max_score=settings.MAX_SCORE, **output)
-    except ValidationError as err:
-        twilio.send_message(msg.sender, messages.INVALID)
-        error_messages: list[str] = [err.get("msg") for err in err.errors()]
-        detail = {"llm_output": output, "error_messages": error_messages}
-        logging.error(json.dumps(detail))
-        raise HTTPException(status.HTTP_400_BAD_REQUEST, detail)
-    sheet.record_match(values=snooker_match.model_dump(), passage=msg.body, sender=msg.sender)
-    for break_ in snooker_match.breaks:
-        sheet.record_break(break_.model_dump(), passage=msg.body, sender=msg.sender)
-    reply = snooker_match.summary(snooker_match.passage_language)
-    twilio.send_message(msg.sender, reply)
-    reply_msg = "Match tested" if msg.is_test else "Match recorded"
-    content = {"status": reply_msg, "match": jsonable_encoder(snooker_match)}
-    logging.info(json.dumps(content))
-    return JSONResponse(status_code=status.HTTP_201_CREATED, content=content)
-
-
 @app.get("/sheet")
 async def redirect_to_sheet(settings=Depends(SETTINGS)):
     """Redirects to the Google Sheet, tab corresponding to current round."""
@@ -112,13 +115,6 @@ async def redirect_to_sheet_sixred24():
     settings = get_settings(sixred24=True)
     sheet = SnookerSheet(settings.SHEETID)
     return RedirectResponse(url=sheet.url)
-
-
-@app.exception_handler(Exception)
-async def handle_exception(req: Request, exc: Exception):
-    logging.exception(exc)
-    if not isinstance(exc, HTTPException):
-        return JSONResponse(status_code=500, content={"detail": "Internal server error"})
 
 
 setup_logging()
