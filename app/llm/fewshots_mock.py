6f5904917dd1a5935c6f6e7cbf44b4d3735270c7
Otto Ahonen <otto@priimus.consulting>
2025-01-05 12:31:15 +0200

    revert llm to old way

--- app/llm/fewshots_mock.py
+++ app/llm/fewshots_mock.py
@@ -1,160 +1,140 @@
-"""This module serves two purposes:
+import json
+from functools import partial
 
-1) Example data for LLM few-shot prompting used in production invocations
-2) TTD scaffolding for testing LLM prompting
-"""
+from ..models import SnookerPlayer
+from . import FewShotData
 
-import random
-from dataclasses import asdict, dataclass
-from typing import Optional
-
-from app.llm.inference import FixtureCollection, SnookerScoresLLM
-from app.models import MatchFixture, SnookerBreak, SnookerMatch, SnookerPlayer
+to_json = partial(json.dumps, ensure_ascii=False)
 
 
-def generate_match_id(length: int = 5) -> str:
-    """Generate a random ID for the fixture."""
-    #                             exclude ambiguous characters
-    return "".join(random.choices("abcdefghjkmnpqrstuvwxyz23456789", k=length))
-
-
-MOCK_PLAYERS = [
-    SnookerPlayer(name="Huhtala Katja", group="L1"),
-    SnookerPlayer(name="Andersson Leila", group="L1"),
-    SnookerPlayer(name="Huuskonen Alexandra", group="L1"),
-    SnookerPlayer(name="Suhonen Tanja", group="L1"),
-    SnookerPlayer(name="Laaksonen Sinikka", group="L2"),
-    SnookerPlayer(name="Tuomi Kari", group="L2"),
-    SnookerPlayer(name="Jauhiainen Mari", group="L2"),
-    SnookerPlayer(name="Lankinen Elisabet", group="L2"),
-    SnookerPlayer(name="Lahti Mika", group="L3"),
-    SnookerPlayer(name="Kari Yrjö", group="L3"),
-    SnookerPlayer(name="Sjöblom Aukusti", group="L3"),
-    SnookerPlayer(name="Kivinen Jarmo", group="L3"),
-    SnookerPlayer(name="Tähtinen Anneli", group="L4"),
-    SnookerPlayer(name="Saarela Tero", group="L4"),
-    SnookerPlayer(name="Pulkkinen Valtteri", group="L4"),
-    SnookerPlayer(name="Eskelinen Tapio", group="L4"),
-]
-
-FIXTURES = FixtureCollection.from_players(MOCK_PLAYERS)
-LLM = SnookerScoresLLM(
-    target_model=SnookerMatch,
-    fixtures=FIXTURES.fixtures,
-)
-
-
-@dataclass
-class InferenceExample:
-    _llm = LLM
-
-    passage: str
-    match: SnookerMatch
-
-    def fixtures_csv(self) -> str:
-        return FIXTURES.as_csv()
+class MockFewShotData(FewShotData):
+    players = [
+        SnookerPlayer(name="Huhtala Katja", group="L1"),
+        SnookerPlayer(name="Andersson Leila", group="L1"),
+        SnookerPlayer(name="Huuskonen Alexandra", group="L1"),
+        SnookerPlayer(name="Suhonen Tanja", group="L1"),
+        SnookerPlayer(name="Laaksonen Sinikka", group="L2"),
+        SnookerPlayer(name="Tuomi Kari", group="L2"),
+        SnookerPlayer(name="Jauhiainen Mari", group="L2"),
+        SnookerPlayer(name="Lankinen Elisabet", group="L2"),
+        SnookerPlayer(name="Lahti Mika", group="L3"),
+        SnookerPlayer(name="Kari Yrjö", group="L3"),
+        SnookerPlayer(name="Sjöblom Aukusti", group="L3"),
+        SnookerPlayer(name="Kivinen Jarmo", group="L3"),
+        SnookerPlayer(name="Tähtinen Anneli", group="L4"),
+        SnookerPlayer(name="Saarela Tero", group="L4"),
+        SnookerPlayer(name="Pulkkinen Valtteri", group="L4"),
+        SnookerPlayer(name="Eskelinen Tapio", group="L4"),
+    ]
 
     @property
-    def llm_expected(self) -> dict:
-        """The example JSON output for the LLM is derived from this."""
-        f_id = FIXTURES.get_fixture_id_by_players(self.match.players)
-        return {
-            "f_id": f_id,
-            "group": self.match.group,
-            "players": self.match.players,
-            "scores": self.match.scores,
-            "breaks": [SnookerBreak(**brk).model_dump() for brk in self.match.breaks],
-        }
-
-    @property
-    def expected(self) -> dict:
-        """The expected output from the LLM."""
-        m = self.match
-        return self.llm_expected | {
-            "winner": m.winner,
-            "loser": m.players[0] if m.players[1] == m.winner else m.players[1],
-            "winner_score": m.scores[0] if m.players[0] == m.winner else m.scores[1],
-            "loser_score": m.scores[1] if m.players[0] == m.winner else m.scores[0],
-        }
-
-    def infer(self) -> SnookerMatch:
-        """Run inference based on the passage."""
-        # can't include self in examples, that would be cheating.
-        apriori_examples = [e for e in EXAMPLES if e != self]
-        return self._llm.infer_match(
-            passage=self.passage,
-            examples=[e.dict() for e in apriori_examples],
-        )
-
-    def test(self):
-        inferred: SnookerMatch = self.infer()
-        assert inferred.f_id == self.expected["f_id"]
-        assert inferred.group == self.expected["group"]
-        assert inferred.player1 in self.expected["players"]
-        assert inferred.player2 in self.expected["players"]
-        assert inferred.winner == self.expected["winner"]
-        assert inferred.loser == self.expected["loser"]
-        assert inferred.winner_score == self.expected["winner_score"]
-        assert inferred.loser_score == self.expected["loser_score"]
-        assert inferred.breaks == self.expected["breaks"]
-
-    def dict(self):
-        """Return dataclass as dictionary"""
-        # TODO: clean this structure up
-        return {"passage": self.passage} | {"expected": self.llm_expected, "fixtures": self.fixtures_csv()}
-
-
-EXAMPLES = [
-    InferenceExample(
-        match=SnookerMatch(
-            passage="Aukusti v Yrjö 2-1, breikit Aukusti 25, Yrjö 18",
-            group="L3",
-            players=("Sjöblom Aukusti", "Kari Yrjö"),
-            scores=(2, 1),
-            winner="Sjöblom Aukusti",
-            breaks=[
-                {"player": "Sjöblom Aukusti", "points": 25},
-                {"player": "Kari Yrjö", "points": 18},
-            ],
-        )
-    ),
-    InferenceExample(
-        match=SnookerMatch(
-            passage="Huhtala - Andersson 2-0. Breikki 45, Huhtala.",
-            group="L1",
-            players=("Huhtala Katja", "Andersson Leila"),
-            scores=(2, 0),
-            winner="Huhtala Katja",
-            breaks=[{"player": "Huhtala Katja", "points": 45}],
-        )
-    ),
-    InferenceExample(
-        match=SnookerMatch(
-            passage="Kari 0 - 2 Sinikka",
-            group="L2",
-            players=("Laaksonen Sinikka", "Tuomi Kari"),
-            scores=(2, 0),
-            winner="Laaksonen Sinikka",
-            breaks=[],
-        )
-    ),
-    InferenceExample(
-        match=SnookerMatch(
-            passage="Tähtinen 1 - Tero 2, ei breikkejä",
-            group="L4",
-            players=("Saarela Tero", "Tähtinen Anneli"),
-            scores=(2, 1),
-            winner="Tähtinen Anneli",
-            breaks=[],
-        )
-    ),
-]
-
-
-def examples_asdicts() -> list[dict]:
-    return [ex.dict() for ex in EXAMPLES]
-
-
-if __name__ == "__main__":
-    for example in EXAMPLES:
-        example.test()
+    def examples(self):
+        return [
+            {
+                "valid_players": "\n".join([str(plr) for plr in self.players]),
+                "passage": "Huhtala - Andersson 2-1. Breikki 45, Huhtala.",
+                "output": to_json(
+                    {
+                        "group": "L1",
+                        "player1": "Huhtala Katja",
+                        "player2": "Andersson Leila",
+                        "player1_score": 2,
+                        "player2_score": 1,
+                        "winner": "Huhtala Katja",
+                        "breaks": [
+                            {
+                                "player": "Huhtala Katja",
+                                "points": 45,
+                            }
+                        ],
+                        "language": "fin"
+                    }
+                ),
+            },
+            {
+                "valid_players": "\n".join([str(plr) for plr in self.players]),
+                "passage": "Sinikka - Joonas 2-0",
+                "output": to_json(
+                    {
+                        "group": "L2",
+                        "player1": "Laaksonen Sinikka",
+                        "player2": "Tuomi Joonas",
+                        "player1_score": 2,
+                        "player2_score": 0,
+                        "winner": "Laaksonen Sinikka",
+                        "breaks": [],
+                        "language": "fin"
+                    }
+                ),
+            },
+            {
+                "valid_players": "\n".join([str(plr) for plr in self.players]),
+                "passage": "Valtteri v Anneli 2-1, breaks: Anneli 107, 101, Valtteri 52",
+                "output": to_json(
+                    {
+                        "group": "L4",
+                        "player1": "Pulkkinen Valtteri",
+                        "player2": "Tähtinen Anneli",
+                        "player1_score": 2,
+                        "player2_score": 1,
+                        "winner": "Pulkkinen Valtteri",
+                        "breaks": [
+                            {
+                                "player": "Tähtinen Anneli",
+                                "points": 107,
+                            },
+                            {
+                                "player": "Tähtinen Anneli",
+                                "points": 101,
+                            },
+                            {
+                                "player": "Pulkkinen Valtteri",
+                                "points": 52,
+                            },
+                        ],
+                        "language": "eng"
+                    }
+                ),
+            },
+            {
+                "valid_players": "\n".join([str(plr) for plr in self.players]),
+                "passage": "Aukusti v Yrjö 2-1, breikit Aukusti 25, Yrjö 18",
+                "output": to_json(
+                    {
+                        "group": "L3",
+                        "player1": "Sjöblom Aukusti",
+                        "player2": "Väisänen Yrjö",
+                        "player1_score": 2,
+                        "player2_score": 1,
+                        "winner": "Sjöblom Aukusti",
+                        "breaks": [
+                            {
+                                "player": "Sjöblom Aukusti",
+                                "points": 25,
+                            },
+                            {
+                                "player": "Väisänen Yrjö",
+                                "points": 18,
+                            },
+                        ],
+                        "language": "fin"
+                    }
+                ),
+            },
+            {
+                "valid_players": "\n".join([str(plr) for plr in self.players]),
+                "passage": "Ahonen 2 - Tero 1, ei breikkejä",
+                "output": to_json(
+                    {
+                        "group": "L4",
+                        "player1": "Tähtinen Anneli",
+                        "player2": "Saarela Tero",
+                        "player1_score": 2,
+                        "player2_score": 1,
+                        "winner": "Tähtinen Anneli",
+                        "breaks": [],
+                        "language": "fin"
+                    }
+                ),
+            },
+        ]
---
+++ app/llm/prompts.py
@@ -0,0 +1,59 @@
+
+"""Prompt and few-shot example generation."""
+
+import logging
+
+from langchain.prompts.few_shot_with_templates import FewShotPromptWithTemplates
+from langchain.prompts.prompt import PromptTemplate
+
+try:
+    from . import fewshots_groove
+    few_shot_data = fewshots_groove.GrooveFewShotData()
+except ImportError:
+    logging.warning("Could not import real player names, using fake few-shot data instead.")
+    from . import fewshots_mock
+    few_shot_data = fewshots_mock.MockFewShotData()
+
+
+
+def get_prompt():
+    """Generates prompt to LLM containing instruction, few-shot examples and placeholder for user input."""
+
+    prompt_prefix = """The following passage contains the outcome of a snooker match.
+    The passage is about a match between two players containing frames won by each player and, optionally, any notable
+    breaks.
+
+    The passage should only ever contain information pertaining to valid players. Below is a list of full names of valid
+    players and their associated groups. Only ever return names of players that are included in the list of valid
+    players. Only ever return names EXACTLY as they appear in the list of valid players, including order of names.
+
+    Players belong to different groups. A match should only ever be between players in the same group. If there is a
+    player with the same name in a different group, assume that the player in the passage is the one in the same group
+    as the other player in the match.
+
+    If a break is not explicitly mentioned in the passage, return an empty list for breaks.
+
+    Also, if it seems certain that the passage is in English, output `language` as "eng", otherwise assume "fin".
+    """
+
+    return FewShotPromptWithTemplates(
+        examples=few_shot_data.examples,
+        example_prompt=PromptTemplate(
+            template="Valid players:\n{{ players_list }}\n\nPassage: {{ passage }}\n\nJSON: {{ output }}\n",
+            input_variables=["players_list", "passage", "output"],
+            template_format="jinja2",
+        ),
+        input_variables=["players_list", "passage"],
+        prefix=PromptTemplate(template=prompt_prefix, input_variables=[], template_format="jinja2"),
+        suffix=PromptTemplate(
+            template="Valid players:\n{{ players_list }}\n\nPassage: {{ passage }}\n\nJSON:",
+            input_variables=["players_list", "passage"],
+            template_format="jinja2",
+        ),
+        template_format="jinja2",
+    )
+
+
+if __name__ == "__main__":
+    # test prompt generation
+    print(get_prompt().format(players_list="foo", passage="bar"))
